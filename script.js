6a93ef8d091adb6673491fc66ca7eaf71cc988ef
AndrasE <rohadtsajt@gmail.com>
2025-02-26 18:34:30 +0000

    form submission functionality w mailto
    in order to work needed to change browser behaviour:
    
    disable adblock
    chrome://settings/handlers
    ->allow Sites can ask to handle protocols
    open console
    ->allow pasting
    -> copy paste:
    navigator.registerProtocolHandler('mailto', 'https://mail.google.com/mail/?extsrc=...%s', 'Gmail')
    ->press allow on popup

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-document.querySelector("form").addEventListener("click", function () {
-  console.log("clicked");
+document.getElementById("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let card = document.getElementById("card").value;
+  let targetEmail = "rohadtsajt@gmail.com";
+  let subject = "Form Submission - Software Dev Challenge";
+  let body = `Name: ${name}%0D%0AEmail: ${email}%0D%0ACard: ${card}`;
+
+  let mailtoLink = `mailto:${targetEmail}?subject=${subject}&body=${body}`;
+
+  window.location.href = mailtoLink;
 });
